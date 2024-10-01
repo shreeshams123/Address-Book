f6c3f9875f817eac7f39d5d4b680a50d924481b9
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-10-01 22:20:26 +0530

    ADDED Main class

---
+++ AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_Project
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book program");
+        }
+    }
+}
